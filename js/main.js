4ea149ff416d03d28a5f92fce7689645244c5802
Mini <makh0019@algonquinlive.com>
2017-05-15 12:21:49 -0400

    week-2-js-file

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*global alert, console*/
+
+//TASK 1
+var a = 3;
+var b = 5;
+var c;
+alert("var a = 3;\nvar b = 5;\nvar c;\n------------\n" +
+      "a + b = " + (a + b) + "\n" +
+      "a - b = " + (a - b) + "\n" +
+      "a * b = " + (a * b) + "\n" +
+      "a / b = " + (a / b) + "\n" +
+      "a % b = " + (a % b) + "\n" +
+      "(a += b): " + (a += b) + "\n" +
+      "(a -= b): " + (a -= b) + "\n" +
+      "(a *= b): " + (a *= b) + "\n" +
+      "(a /= b): " + (a /= b) + "\n" +
+      "(a %= b): " + (a %= b) + "\n" +
+      "(a == b): " + (a == b) + "\n" +
+      "(a != b): " + (a != b) + "\n" +
+      "(a > b): " + (a > b) + "\n" +
+      "(a < b): " + (a < b) + "\n" +
+      "(!a && !c): " + (!a && !c) + "\n" +
+      "(!a || !c): " + (!a || !c) + "\n"      
+);
+
+
+//TASK 2
+
+var first_name = "Mini";
+var last_name = "Makhija";
+var email = "mini.makhija@gmail.com";
+var output = "My name is " +  "Mini Makhija" + "." + " " + "You can contact me at" + " " + "mini.makhija@gmail.com" + ".";
+alert (output);
